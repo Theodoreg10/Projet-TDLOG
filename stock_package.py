9f1531ed89162557a89f3b2bfc7ef71cb35bdf1a
Mariana Soares <mariretumba@gmail.com>
2023-11-06 15:57:26 +0100

    Stock_package 2

--- stock_package.py
+++ stock_package.py
@@ -1,34 +1,75 @@
-from math import *
 # cu : coût unitaire
 # cp : coût passation
 # tp : taux de possetion
 
+from math import sqrt, exp, factorial
+from scipy.stats import norm
 
 # Date fixe et Quantité fixe
-def S1 (demande):
+
+
+def S1(demande):
     return demande
 
+
 # Date fixe et Quantité variable
-def S2(demande , cu , cp , tp):
-    Q_EOQ = sqrt((2*demande * cp )/(tp * cu))
+
+
+def S2(demande, cu, cp, tp):
+    Q_EOQ = sqrt((2 * demande * cp) / (tp * cu))
     return Q_EOQ
 
+
 # Date variable et Quantité fixe [ point de commande ]
-def S3(demande, delai_livraison , temps_consommation):
+
+
+def S3(demande, delai_livraison, temps_consommation):
     D = demande
     DL = delai_livraison
     T = temps_consommation
-    PC = (D)/(T*DL)
+    PC = ((D) / (T)) * DL
     return PC
+
 
 # Date variable et Quantité variable
 
-def lambda_S4 (demande, horizon):
+
+def lambda_S4(demande, horizon):
     D = demande
     HT = horizon
-    lamb = D/HT
+    lamb = D / HT
     return lamb
 
-def S4 (lamb , k):
-    P = (((lamb ** k)/(factorial(k)))) * (exp**(-lamb))
+
+def S4(lamb, k):
+    P = (((lamb**k) / (factorial(k)))) * (exp ** (-lamb))
     return P
+
+
+# Stock de sécurité
+
+
+def StockSec1(demande_moy, delai_livraison):
+    DL = delai_livraison
+    DM = demande_moy
+    SS1 = DL * DM
+    return SS1
+
+
+def StockSec2(niveau_service, ecart_type_demande):
+    ETD = ecart_type_demande
+    NS = niveau_service
+    z = norm.ppf(NS)
+    SS2 = z * ETD
+    return SS2
+
+
+# Vrai stock
+
+
+def stock_final(stock, stock_securité):
+    return stock + stock_securité
+
+
+def stock_alerte(stock_min, stock_securité):
+    return stock_min + stock_securité
--- stockmng/stockmng/settings.py
+++ stockmng/stockmng/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "stockmng.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'template')],
+        "DIRS": [os.path.join(BASE_DIR, "template")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,21 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media')
-]
+MEDIAFILES_DIRS = [os.path.join(BASE_DIR, "media")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
