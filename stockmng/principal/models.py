74b1f17726d152a9e8acb8f84035e472cac9d5a8
Theodore GNIMAVO <theodore.gnimavo@eleves.enpc.fr>
2023-11-13 17:19:30 +0100

    table display

--- stockmng/principal/models.py
+++ stockmng/principal/models.py
@@ -7,6 +7,10 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
     qte_unitaire = models.IntegerField()
+    unit_cost = models.FloatField(default=0.0)
+    fixed_command_cost = models.FloatField(default=0.0)
+    holding_rate = models.FloatField(default=0.0)
+    service_level = models.FloatField(default=0.0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- stockmng/principal/views.py
+++ stockmng/principal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product, Sale
 
 # Create your views here.
 
@@ -8,7 +9,13 @@ def handle_home_page(request):
 
 
 def handle_data_page(request):
-    return render(request, "data.html")
+    products = Product.objects.all()
+    sales = Sale.objects.all()
+    products = Product.objects.all()
+    context = {'products': products,
+               'sales': sales
+               }
+    return render(request, "data.html", context)
 
 
 def handle_about_page(request):
