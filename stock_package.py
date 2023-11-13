efef10616f18909b463faa13d584e6768f4dcb66
SarraDhaouadi <sarra.dhaouadi@etudiant-enit.utm.tn>
2023-11-13 14:42:20 +0100

    type variables

--- stock_package.py
+++ stock_package.py
@@ -8,55 +8,57 @@ from scipy.stats import norm
 # Date fixe et Quantité fixe
 
 
-def S1(demande):
+def Scenario1(demande: float):
     return demande
 
 
 # Date fixe et Quantité variable
 
 
-def S2(demande, cu, cp, tp):
-    Q_EOQ = sqrt((2 * demande * cp) / (tp * cu))
-    return Q_EOQ
+def Scenario2(demande: float, cout_unitaire: float, cout_passation: float,
+              taux_possession: float):
+    Quantite_eco_commandee = sqrt(
+        (2 * demande * cout_passation) / (taux_possession * cout_unitaire))
+    return Quantite_eco_commandee
 
 
 # Date variable et Quantité fixe [ point de commande ]
 
 
-def S3(demande, delai_livraison, temps_consommation):
+def Scenario3(demande: float, delai_livraison: int, temps_consommation: int):
     D = demande
     DL = delai_livraison
     T = temps_consommation
-    PC = ((D) / (T)) * DL
-    return PC
+    Point_Commande = ((D) / (T)) * DL
+    return Point_Commande
 
 
 # Date variable et Quantité variable
 
 
-def lambda_S4(demande, horizon):
+def lambda_Scenario4(demande: float, horizon: int):
     D = demande
     HT = horizon
     lamb = D / HT
     return lamb
 
 
-def S4(lamb, k):
-    P = (((lamb**k) / (factorial(k)))) * (exp ** (-lamb))
-    return P
+def Scenario4(lamb: float, k: float):
+    Probabilite = (((lamb**k) / (factorial(k)))) * (exp ** (-lamb))
+    return Probabilite
 
 
 # Stock de sécurité
 
 
-def StockSec1(demande_moy, delai_livraison):
+def StockSecurite1(demande_moy: float, delai_livraison: int):
     DL = delai_livraison
     DM = demande_moy
     SS1 = DL * DM
     return SS1
 
 
-def StockSec2(niveau_service, ecart_type_demande):
+def StockSecurite2(niveau_service: float, ecart_type_demande: float):
     ETD = ecart_type_demande
     NS = niveau_service
     z = norm.ppf(NS)
@@ -67,9 +69,9 @@ def StockSec2(niveau_service, ecart_type_demande):
 # Vrai stock
 
 
-def stock_final(stock, stock_securité):
+def stock_final(stock: float, stock_securité: float):
     return stock + stock_securité
 
 
-def stock_alerte(stock_min, stock_securité):
-    return stock_min + stock_securité
+def stock_alerte(stock_min: float, stock_securite: float):
+    return stock_min + stock_securite
