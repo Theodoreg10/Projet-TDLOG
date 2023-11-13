46f56dd44d57e7ab90f80ef285dbc3247011cb5d
Theodore GNIMAVO <theodore.gnimavo@eleves.enpc.fr>
2023-11-13 15:27:27 +0100

    database added

--- stockmng/principal/admin.py
+++ stockmng/principal/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Sale
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'qte_unitaire', 'user')
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('date', 'quantity', 'ref', 'user')
