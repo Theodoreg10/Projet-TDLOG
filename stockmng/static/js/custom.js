f42179a7b38bf5a6e2f3caa32b55ad76e977b74d
Theodoreg10 <theodoregnimavo6@gmail.com>
2024-01-08 13:31:39 +0100

    style

--- stockmng/static/js/custom.js
+++ stockmng/static/js/custom.js
@@ -31,14 +31,3 @@ $('.slick_slider_nav').slick({
     focusOnSelect: true,
     variableWidth: true
 });
-
-
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
